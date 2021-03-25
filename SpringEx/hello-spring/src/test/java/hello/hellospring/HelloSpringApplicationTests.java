c2e4a131f5cc62588aecab6dd07af7301657672a
Ga-Yeon Park <rkdusdd52@naver.com>
2021-03-25 23:33:04 +0900

    test

--- SpringEx/hello-spring/src/test/java/hello/hellospring/HelloSpringApplicationTests.java
+++ SpringEx/hello-spring/src/test/java/hello/hellospring/HelloSpringApplicationTests.java
@@ -1,13 +1,20 @@
 package hello.hellospring;
 
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+//import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+
 @SpringBootTest
 class HelloSpringApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
