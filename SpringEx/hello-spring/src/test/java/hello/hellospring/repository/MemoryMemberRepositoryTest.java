7d6c22eff4491543e47b2d859659f9cbf2ace927
Ga-Yeon Park <rkdusdd52@naver.com>
2021-03-25 23:03:55 +0900

    testpratice

---
+++ SpringEx/hello-spring/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,13 @@
+package hello.hellospring.repository;
+
+import org.junit.jupiter.api.Test;
+
+public class MemoryMemberRepositoryTest {
+
+    MemberRepository repository = new MemoryMemberRepository();
+
+    @Test
+    public void save(){
+
+    }
+}
