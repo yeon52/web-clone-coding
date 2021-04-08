670e11ebf186bb23545c8568128fdf0d6f0e1cc2
Ga-Yeon Park <rkdusdd52@naver.com>
2021-04-08 23:47:50 +0900

    service

--- SpringEx/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ SpringEx/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -10,25 +10,25 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HelloController {
     @GetMapping("hello")
     public String hello(Model model) {
-        model.addAttribute("data","Spring!!"); //key, value값
-        return "hello"; //resources/templates 속 hello.html 파일을 찾아서 랜더링 해라
+        model.addAttribute("data", "Spring!!"); // key, value값
+        return "hello"; // resources/templates 속 hello.html 파일을 찾아서 랜더링 해라
     }
 
     @GetMapping("hello-mvc")
-    public String helloMvc(@RequestParam("name") String name, Model model){
+    public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
-        return "hello-template";
+        return "hello-template"; // viewResolver에게 주고, viewResolver가 html로 반환 후 내려줌(템플릿엔진 처리)
     }
 
     @GetMapping("hello-string")
-    @ResponseBody //http body부분에 데이터를 직접 넣어 주겠다.
-    public String helloString(@RequestParam("name") String name){
-        return "hello "+name; //->템플릿엔진과 다르게 데이터'만' 내려줌
+    @ResponseBody // http body부분에 데이터를 직접 넣어 주겠다.
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; // ->템플릿엔진과 다르게 데이터'만' 내려줌
     }
 
-    @GetMapping("hello-api") //json 방식
-    @ResponseBody //객체를 반환하고 ResponseBody사용 시 json으로 반환이 default
-    public Hello helloApi(@RequestParam("name") String name){
+    @GetMapping("hello-api") // json 방식
+    @ResponseBody // 객체를 반환하고 ResponseBody사용 시 json으로 반환이 default
+    public Hello helloApi(@RequestParam("name") String name) {
         Hello hello = new Hello();
         hello.setName(name);
         return hello;
